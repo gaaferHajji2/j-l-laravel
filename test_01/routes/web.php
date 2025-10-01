1bc03d8def62deac3c30316bdec20b81ea2ba4d8
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-01 22:27:08 +0300

    set default values

--- test_01/routes/web.php
+++ test_01/routes/web.php
@@ -2,11 +2,21 @@
 
 use App\Services\DeployApp;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/img', function() {
+
+    $url = Storage::temporaryUrl('Sword_02.jpg', now()->addSeconds(10));
+
+    return response()->json([
+        'url' => $url
+    ]);
+
+});
 
 Route::get('/demo', function(DeployApp $deploy) {
 
