5fd23bca46ed375db57d4cdedc4593731858f876
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 14:51:00 +0300

    Create books migration

---
+++ jloka-app/database/migrations/2026_01_20_115014_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('isbn')->unique();
+            $table->date('published_date');
+            $table->string('writer_id'); // Custom foreign key
+            
+            // Custom foreign key constraint
+            $table->foreign('writer_id')
+                  ->references('author_slug')
+                  ->on('authors')
+                  ->onDelete('cascade');
+                  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
