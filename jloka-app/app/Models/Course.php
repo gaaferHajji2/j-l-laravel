94a354de728fbdf8c5a5b37fb57289832806bd5e
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 14:40:31 +0300

    Create course model

---
+++ jloka-app/app/Models/Course.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    protected $primaryKey = 'course_identifier';
+    public $incrementing = false;
+    
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'course_enrollments',
+            'course_code',         // Foreign key on pivot table for course
+            'student_reg_number',  // Foreign key on pivot table for student
+            'course_identifier',   // Local key on course table
+            'student_reg_no'       // Local key on student table
+        )->withPivot(['enrollment_date', 'grade', 'status'])
+        ->withTimestamps()
+        ->using(CourseEnrollment::class);
+    }
+
+}
