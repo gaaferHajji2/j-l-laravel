99579c92400aad7c6f9d601ada47c2b0ac19c332
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-15 09:29:42 +0300

    Build Permissions Controller

---
+++ jloka-cache-roles-and-permissions/app/Http/Controllers/PermissionsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+class PermissionsController extends Controller
+{
+    public function assignPermissionToUser(Request $request)
+    {
+        $user = User::findOrFail($request->user_id);
+
+        $user->givePermissionTo($request->permission);
+
+        return response()->json([
+            'message' => 'Permission assigned'
+        ]);
+    }
+
+    public function revokePermissionFromUser(Request $request)
+    {
+        $user = User::findOrFail($request->user_id);
+
+        $user->revokePermissionTo($request->permission);
+
+        return response()->json([
+            'message' => 'Permission revoked'
+        ]);
+    }
+
+    public function assignPermissionToRole(Request $request)
+    {
+        $role = Role::findByName($request->role);
+
+        $role->givePermissionTo($request->permission);
+
+        return response()->json([
+            'message' => 'Permission assigned to role'
+        ]);
+    }
+}
--- jloka-cache-roles-and-permissions/app/Models/User.php
+++ jloka-cache-roles-and-permissions/app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
-
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
