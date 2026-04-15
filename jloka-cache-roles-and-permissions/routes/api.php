0a9617c56fd9d79c03bafffc0c2daae40c2b353a
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-15 10:04:23 +0300

    Adding product routes

--- jloka-cache-roles-and-permissions/routes/api.php
+++ jloka-cache-roles-and-permissions/routes/api.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Every path has explicit name() and middleware() attributes
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+    Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+
+    Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
