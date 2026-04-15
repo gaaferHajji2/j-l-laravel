a0f57418af4ea9a6ed349b863a2e0054e176c8c2
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-15 09:48:57 +0300

    Adding pbinding between product interface & repository

--- jloka-cache-roles-and-permissions/app/Providers/AppServiceProvider.php
+++ jloka-cache-roles-and-permissions/app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\Interface\ProductRepositoryInterface;
+use App\Repositories\ProductRepository;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            ProductRepositoryInterface::class ,
+            ProductRepository::class
+        );
     }
 
     /**
